c27acaf306ea3e574730faea884698ac1a30573f
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-05-09 11:20:27 +0200

    create DirectoryList Class

---
+++ src/main/java/io/alecrim/itacademy/sprint1/task5/Main.java
@@ -0,0 +1,9 @@
+package io.alecrim.itacademy.sprint1.task5;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String directoryPath = "src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise1/alphabetList.txt";
+            DirectoryList.getContentList(directoryPath);
+    }
+}
--- src/test/java/io/alecrim/itacademy/sprint1/task4/n2exercise1/TestStrings.java
+++ src/test/java/io/alecrim/itacademy/sprint1/task4/n2exercise1/TestStrings.java
@@ -4,11 +4,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 
-public class TestStrings {
-
-    @Test
-    public void testStringLength() {
-        String message = "Mordor";
-        assertThat(message.length(), equalTo(8));
-    }
-}
+//public class TestStrings {
+//
+//    @Test
+//    public void testStringLength() {
+//        String message = "Mordor";
+//        assertThat(message.length(), equalTo(8));
+//    }
+//}
