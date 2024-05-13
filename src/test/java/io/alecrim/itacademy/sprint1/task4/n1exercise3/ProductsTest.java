a31ba20b1aaf40d3c529f2208896b02dd718b9ff
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-05-13 13:39:00 +0200

    fix exercise3 task 3 - Parameterized Test

--- src/test/java/io/alecrim/itacademy/sprint1/task4/n1exercise3/ProductsTest.java
+++ src/test/java/io/alecrim/itacademy/sprint1/task4/n1exercise3/ProductsTest.java
@@ -1,16 +1,28 @@
 package io.alecrim.itacademy.sprint1.task4.n1exercise3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class ProductsTest {
 
-    @Test
-    void checkIndexOutOfBoundsThrowsError() {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 4, 6})
+
+    void checkIndexOutOfBoundsThrowsError(int index) {
         Products products = new Products();
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            products.getSpecificProduct(6);
+
+        IndexOutOfBoundsException exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            products.getSpecificProduct(index);
         });
+
+        String expectedMessage = "Index out of bounds: Index " + index + " out of bounds for length " + products.allProducts.length;
+        String actualMessage = exception.getMessage();
+
+        System.out.println("Expected exception message to contain: " + expectedMessage + ", but got: " + actualMessage);
+        assertTrue(actualMessage.contains(expectedMessage), "Expected exception message to contain: " + expectedMessage + ", but got: " + actualMessage);
     }
 }
