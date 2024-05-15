214d1871d1aec4edb3f0c6aafc17eb96635fe3df
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-05-15 10:37:49 +0200

    complete exercise5 task 5 - serializable object

--- src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise5/Main.java
+++ src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise5/Main.java
@@ -1,4 +1,16 @@
 package io.alecrim.itacademy.sprint1.task5.n1exercise5;
 
 public class Main {
+    public static void main(String[] args) {
+        String data = "Serialized object";
+        SerializableObject objectToSerialize = new SerializableObject(data);
+
+        String filePath = "serialized_object.ser";
+        SerializableObject.serializeObject(objectToSerialize, filePath);
+
+        SerializableObject deserializedObject = SerializableObject.deserializeObject(filePath);
+        if (deserializedObject != null) {
+            System.out.println("Deserialized object data: " + deserializedObject.getData());
+        }
+    }
 }
---
+++ src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise5/SerializableObject.java
@@ -0,0 +1,34 @@
+package io.alecrim.itacademy.sprint1.task5.n1exercise5;
+import java.io.*;
+
+public class SerializableObject implements Serializable {
+    private String data;
+
+    public SerializableObject(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public static void serializeObject(SerializableObject object, String filePath) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            objectOutputStream.writeObject(object);
+            System.out.println("Object serialized successfully.");
+        } catch (IOException e) {
+            System.out.println("Error occurred during serialization: " + e.getMessage());
+        }
+    }
+
+    public static SerializableObject deserializeObject(String filePath) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+            SerializableObject object = (SerializableObject) objectInputStream.readObject();
+            System.out.println("Object deserialized successfully.");
+            return object;
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error occurred during deserialization: " + e.getMessage());
+            return null;
+        }
+    }
+}
