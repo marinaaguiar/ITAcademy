b6f6ffde276e266a0be3288fd5fea100a3166487
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-05-22 12:40:25 +0200

    review task5 exercise1 - add exception validation

--- src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise1/DirectoryList.java
+++ src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise1/DirectoryList.java
@@ -1,18 +1,18 @@
 package io.alecrim.itacademy.sprint1.task5.n1exercise1;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 public class DirectoryList {
 
-    public static void getContentList(String filePath) {
+    public static void getContentList(String filePath) throws FileNotFoundException {
         File directory = new File(filePath);
 
         String[] contents = directory.list();
         Arrays.sort(contents);
 
         if (!directory.isDirectory()) {
-            System.out.println("The provided path is not a directory.");
-            return;
+            throw new FileNotFoundException("The provided path is not a directory.");
         }
 
         for (String content : contents) {
--- src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise1/Main.java
+++ src/main/java/io/alecrim/itacademy/sprint1/task5/n1exercise1/Main.java
@@ -1,9 +1,15 @@
 package io.alecrim.itacademy.sprint1.task5.n1exercise1;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
         String directoryPath = args[0];
-        DirectoryList.getContentList(directoryPath);
+        try {
+            DirectoryList.getContentList(directoryPath);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
